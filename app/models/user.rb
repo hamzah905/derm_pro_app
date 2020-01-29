ba9b8cccb5ca392b3d7717da33b4210d684a38a6
Hamza Satti <hamza.satti159@gmail.com>
2020-01-29 16:04:18 +0500

    Add roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,16 @@ class User < ApplicationRecord
 
   # Validations
   # validates_presence_of :email
+  enum role: [:patient, :doctor]
   validates :email, uniqueness: true
 
   def is_social?
     self.SocialLogIn.eql? true
+  end
+
+  after_initialize do
+    if self.new_record?
+      self.role ||= :patient
+    end
   end
 end
