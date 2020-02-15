15c1fe25d828468be07502f02aa2e0637c872d0d
Hamza Satti <hamza.satti159@gmail.com>
2020-02-16 01:44:55 +0500

    Fix confirm

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,8 +1,13 @@
 class ConfirmationsController < Devise::ConfirmationsController
   # private
-  def show
-    # sign_in(resource) # In case you want to sign in the user
-    redirect_to home_account_verification_path
-    return
+  def show 
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      redirect_to home_account_verification_path
+    end
   end
+  
 end
