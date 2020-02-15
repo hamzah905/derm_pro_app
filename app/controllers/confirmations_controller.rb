08cf4e5c13c300d51547a2a08f391225a4041871
Hamza Satti <hamza.satti159@gmail.com>
2020-02-16 01:20:21 +0500

    Fix confirm

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,7 @@
 class ConfirmationsController < Devise::ConfirmationsController
-  private
-  def after_confirmation_path_for(resource_name, resource)
+  
+  protected
+  def do_confirm
     # sign_in(resource) # In case you want to sign in the user
     redirect_to home_account_verification_path
   end
