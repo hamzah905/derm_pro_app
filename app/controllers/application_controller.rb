5f7dcef9583a0006e4de324d2b024de52e25635f
Hamza Satti <hamza.satti159@gmail.com>
2020-02-28 01:43:58 +0500

    Fix CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
 	rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_error
 
 	private
