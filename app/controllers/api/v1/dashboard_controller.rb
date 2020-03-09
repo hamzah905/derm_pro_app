143da8acc58f86d7c67370c8240f7c8bf308e5d6
Hamza Satti <hamza.satti159@gmail.com>
2020-03-09 19:12:24 +0500

    Fix graph api

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -4,10 +4,14 @@ class Api::V1::DashboardController < Api::V1::BaseController
     user_per_day = {}
     ["1", "2", "3","4","5","6"].each_with_index do |day, index|
       @date = (DateTime.now) - ((index + 1) * 7).days
-      @users = User.where(:created_at => @date..DateTime.now - (index * 7).days)
+      @users = User.where(role: "patient", :created_at => @date..DateTime.now - (index * 7).days)
       user_per_day[day] = @users.count
     end
-    response = { message: "User Per Day", user: user_per_day, auth_token: auth_token }
+    doctor_subscription_rate = (User.where(role: "doctor", is_activated: true).count * 100) / User.where(role: "doctor").count
+    # debugger
+    pending_qs = QuerySpot.joins("left join feedbacks on feedbacks.query_spot_id = query_spots.id").where("feedbacks.query_spot_id is NOT null")
+    reports_pending_ratio = (pending_qs.count * 100)/QuerySpot.count
+    response = { message: "User Per Day", user: {user_per_day: user_per_day, doctor_subscription_rate: doctor_subscription_rate, reports_pending_ratio: reports_pending_ratio}, auth_token: auth_token }
     json_response(response)
   end
 
