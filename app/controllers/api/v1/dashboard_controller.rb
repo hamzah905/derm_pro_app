c6dfc64ed8876548197180f5ae9cc7a49e8fdde5
Hamza Satti <hamza.satti159@gmail.com>
2020-03-09 18:46:51 +0500

    Fix bug

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::DashboardController < Api::V1::BaseController
 
   def dashboard_graphs_user
     user_per_day = {}
-    ["1", "2", "3","4","5","6","7","8","9","10","11","12"].each_with_index do |day, index|
+    ["1", "2", "3","4","5","6"].each_with_index do |day, index|
       @date = (DateTime.now) - ((index + 1) * 7).days
       @users = User.where(:created_at => @date..DateTime.now - (index * 7).days)
       user_per_day[day] = @users.count
