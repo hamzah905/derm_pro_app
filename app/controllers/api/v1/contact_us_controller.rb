92e99339b68e04d543a43346cf589bbe23e851bb
Hamza Satti <hamza.satti159@gmail.com>
2020-03-11 21:09:50 +0500

    Bug fix

--- app/controllers/api/v1/contact_us_controller.rb
+++ app/controllers/api/v1/contact_us_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::ContactUsController < Api::V1::BaseController
+  skip_before_action :authorize_request, only: [ :create]
 
   def create
-    contact_us = current_user.contacts.create!(contact_us_params)
+    contact_us = ContactUs.create!(contact_us_params)
     response = { message: "contact_us created successfully", contact_us: contact_us, auth_token: auth_token }
     json_response(response)
   end
