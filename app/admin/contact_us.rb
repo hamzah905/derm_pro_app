814c1ac6a9b0c2ada8e796d0e1107c019fb77d9b
Hamza Satti <hamza.satti159@gmail.com>
2020-03-12 12:39:22 +0500

    Added contact us activeadmin resource

---
+++ app/admin/contact_us.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register ContactUs do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :description, :user_id
+  #
+  
+  controller do
+    def action_methods
+      super - ['new', 'create', 'edit', 'update']
+    end
+  end
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :description, :user_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
