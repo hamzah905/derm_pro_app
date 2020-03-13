539f8f7cc9369dd2e1a4fc62196a6dfae28cd7b7
Hamza Satti <hamza.satti159@gmail.com>
2020-03-13 17:56:23 +0500

    Fix time zone issue

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(*Rails.groups)
 module DermProApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Islamabad'
+
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
