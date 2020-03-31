550db6ca961758373a11285ab605148f460d16df
Hamza Satti <hamzah905>
2020-03-31 20:38:12 +0500

    bug fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :dob, :contact_no, :role, :gender, :confirmation_code, :email_verified, :avatar, :user_quizes, :number_verified, :all_quizes
+  attributes :id, :first_name, :last_name, :email, :dob, :contact_no, :role, :gender, :confirmation_code, :email_verified, :avatar, :user_quizes, :number_verified, :all_quizes, :is_activated
 
   def email_verified
     object.confirmed_at.present? ? true : false
