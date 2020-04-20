c21fd6a1d1c926be06bd52b29806a2b31f21c51c
Hamza Satti <hamzah905>
2020-04-20 19:50:36 +0500

    Sorting of users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :dob, :contact_no, :role, :gender, :confirmation_code, :email_verified, :avatar, :user_quizes, :number_verified, :all_quizes, :is_activated, :doctor_type
+  attributes :id, :first_name, :last_name, :email, :dob, :contact_no, :role, :gender, :confirmation_code, :email_verified, :avatar, :user_quizes, :number_verified, :all_quizes, :is_activated, :doctor_type, :created_at
 
   def email_verified
     object.confirmed_at.present? ? true : false
